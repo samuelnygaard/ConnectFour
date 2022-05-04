30b1ce3aba4198380010a6f562a09152fed5a00a
buus <simon@ambolt.io>
2022-05-04 16:13:41 +0200

    Making a start page

--- app/modules/game/controllers.py
+++ app/modules/game/controllers.py
@@ -4,7 +4,7 @@ mod = Blueprint('game', __name__, url_prefix='/')
 
 
 @mod.route('/', methods=['GET'])
-def play():
+def index():
     board = [['-', '-', '-', '-', '-', '-', '-'],
              ['-', '-', '-', '-', '-', '-', '-'],
              ['-', '-', '-', '-', '-', 'O', '-'],
@@ -12,4 +12,23 @@ def play():
              ['O', 'X', '-', 'O', '-', 'X', '-'],
              ['X', 'O', '-', 'X', 'X', 'O', 'X']]
 
-    return render_template('index.html', board=board)
+    return render_template('game/index.html', board=board)
+
+
+@mod.route('/play', methods=['GET'])
+def play():
+    example_board_1 = [['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', 'O', '-'],
+                       ['-', 'X', '-', '-', '-', 'O', '-'],
+                       ['O', 'X', '-', 'O', '-', 'X', '-'],
+                       ['X', 'O', '-', 'X', 'X', 'O', 'X']]
+
+    example_board_2 = [['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-']]
+
+    return render_template('game/play.html', board=example_board_2)
